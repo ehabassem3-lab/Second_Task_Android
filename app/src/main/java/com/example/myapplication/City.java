aaed07d9c7ec57389799602e9c0511c5c16bea06
ehabassem3.lab <you@example.com>
2025-12-20 17:55:57 +0200

    first commit

---
+++ app/src/main/java/com/example/myapplication/City.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class City {
+     public  String Name ;
+     public  int ImageUrl;
+    public City(String name , int imageUrl){
+        Name = name;
+        ImageUrl = imageUrl;
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/CityAdapter.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.City;
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+public class CityAdapter {
+
+
+}
